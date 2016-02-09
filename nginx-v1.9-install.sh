1470aab28bef37d1a700a0a47e5b4db255c5babe
Alex Markessinis <markea@almyz125.com>
2016-02-09 08:04:21 -0500

    fixed issues with init script installation

--- nginx-v1.9-install.sh
+++ nginx-v1.9-install.sh
@@ -8,17 +8,19 @@ function die {
 # Version of nginx to install
 VERSION=1.9.10
 # Install build environment
-sudo apt-get install build-essential zlib1g-dev libpcre3-dev libssl-dev unzip libssl-dev libxslt1-dev libxml2-dev libgd2-xpm-dev libgeoip-dev libgoogle-perftools-dev libperl-dev curl chkconfig -y;
+sudo apt-get install build-essential zlib1g-dev libpcre3-dev libssl-dev unzip libssl-dev libxslt1-dev libxml2-dev libgd2-xpm-dev libgeoip-dev libgoogle-perftools-dev libperl-dev curl unzip atool chkconfig -y;
 # Remove apt-version of nginx
-sudo apt-get remove --purge nginx* -y;
+sudo apt-get remove --purge nginx -y;
 # Stop Nginx if it is installed from source.
 sudo service nginx stop;
+# Crerate Temp installer dir
+sudo mkdir -p /tmp/NginxInstaller;
 # Get nginx init script
-curl -o /tmp/nginx-init.zip https://codeload.github.com/Fleshgrinder/nginx-sysvinit-script/zip/master &&  unzip -o /tmp/nginx-init.zip && rm -rf /tmp/nginx-init.zip;
+cd /tmp/NginxInstaller; curl -o nginx-sysvinit-script.zip https://codeload.github.com/Fleshgrinder/nginx-sysvinit-script/zip/master && aunpack nginx-sysvinit-script.zip; rm nginx-sysvinit-script.zip;
 # Get Nginx Source
-cd /tmp; curl http://nginx.org/download/nginx-$VERSION.tar.gz | tar xvz;
+cd /tmp/NginxInstaller; curl http://nginx.org/download/nginx-$VERSION.tar.gz | tar xvz;
 # Move into nginx src directory.
-cd /tmp/nginx-$VERSION;
+cd /tmp/NginxInstaller/nginx-$VERSION;
 # Configure with the module path
 ./configure --user=nginx --group=nginx --prefix=/usr/share/nginx --sbin-path=/usr/sbin/nginx --conf-path=/etc/nginx/nginx.conf --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --without-http_scgi_module --without-http_uwsgi_module --with-http_gzip_static_module --with-pcre-jit --with-http_ssl_module --with-pcre --with-file-aio --with-http_realip_module --with-http_v2_module;
 # Exit if configure failed
@@ -34,8 +36,7 @@ die "Failed, at make install aborting...";
 # Add the nginx user if it does not already exist
 sudo id -u nginx &>/dev/null || sudo useradd -r nginx;
 # Install NGINX init script
-cd /tmp/nginx-sysvinit-script;
-sudo make;
+cd /tmp/NginxInstaller/nginx-sysvinit-script-master; sudo make;
 # exit if it fails while installing init script
 die "Failed, at make init script aborting...";
 # Have chkconfig monitor nginx's config file
@@ -46,5 +47,4 @@ nginx -v;
 sudo service nginx start;
 sudo service nginx status;
 ## Cleanup
-sudo rm -rf /tmp/nginx-$VERSION;
-sudo rm -rf /tmp/nginx-sysvinit-script;
+sudo rm -rf /tmp/NginxInstaller;
